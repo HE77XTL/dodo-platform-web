28759b036a7b530c4862ff0319768d93962aebe6
何东升 <he77505357@163.com>
2021-03-09 09:48:42 +0800

    jwt 验证

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,13 +1,32 @@
 <template>
     <div>
         about
+        <button @click="getUser" class="ui-button">getUser</button>
     </div>
 </template>
 
 <script lang="ts">
-    import {  defineComponent } from 'vue'
+    import axios from 'axios'
+    import {defineComponent} from 'vue'
+
     export default defineComponent({
         name: 'HelloWorld',
+        data() {
+            return {}
+        },
+        methods: {
+            getUser() {
+                const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImhlZHMiLCJpYXQiOjE2MTUyNTQxOTV9.ToL7ToU2K361VDfp7mESNHXd8Eh2DgKC2E7Lcag0R2k'
+
+                axios.defaults.headers.Authorization = `Bearer ${token}`;
+                axios({
+                    method: 'get',
+                    url: 'http://127.0.0.1:7002/getUser'
+                }).then(res => {
+                    console.log(res)
+                })
+            }
+        }
     })
 </script>
 
--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -1,18 +1,17 @@
 <template>
     <div class="login">
-        login
         <div>
             <label>
-                <input type="text" v-model="userName">
+                <input v-model="userName" class="ui-input">
             </label>
         </div>
         <div>
             <label>
-                <input type="text" v-model="password">
+                <input v-model="password" class="ui-input">
             </label>
         </div>
         <div>
-            <button @click="onLogin">login</button>
+            <button @click="onLogin" class="ui-button" data-type="primary">login</button>
         </div>
     </div>
 </template>
@@ -31,13 +30,14 @@
         },
         methods: {
             onLogin() {
-                console.log(this.userName)
-                console.log(this.password)
                 axios({
                     method: 'get',
-                    url: 'http://127.0.0.1:7002/'
+                    url: 'http://127.0.0.1:7002/login'
                 }).then(res => {
-                    console.log(res)
+                    console.log('token')
+                    console.log(res.data.data)
+                    // axios.defaults.headers.Authorization = res.data.data;
+                    // axios.defaults.headers.Authorization = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImhlZHMiLCJpYXQiOjE2MTUyNTQxOTV9.ToL7ToU2K361VDfp7mESNHXd8Eh2DgKC2E7Lcag0R2k';
                 })
             }
         }
